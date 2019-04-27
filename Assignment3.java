012fcfd95106eb374d52e7ee917a58ae03942b2c
Kasim Tiba <kasibtiba@gmail.com>
2019-04-26 21:10:38 -0700

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,53 @@
+ public class Assignment3
+{
+   public static void main(String[] args)
+   {
+      System.out.println("There was an old woman who swallowed a fly.");
+      lyric();
+      System.out.println();
+      System.out.println("There was an old woman who swallowed a spider,\nThat wriggled and iggled and jiggled inside her." );
+      lyric2();
+      lyric();
+      System.out.println();
+      System.out.println("There was an old woman who swallowed a bird,\nHow absurd to swallow a bird.");
+      lyric4();
+      lyric2();
+      lyric();
+      System.out.println();
+      System.out.println("There was an old woman who swallowed a cat,\nImagine that to swallow a cat.");
+      lyric6();
+      lyric4();
+      lyric2();
+      lyric();
+      System.out.println();
+      System.out.println("There was an old woman who swallowed a dog,\nWhat a hog to swallow a dog.");
+      lyric8();
+      lyric6();
+      lyric4();
+      lyric2();
+      lyric();
+      System.out.println();
+      System.out.println("There was an old woman who swallowed a horse,\nShe died of course.");
+   }
+   public static void lyric()
+   {
+      System.out.println("I don't know why she swallowed that fly,\nPerhaps she'll die.");
+   }
+   public static void lyric2()
+   {
+      System.out.println("She swallowed the spider to catch the fly.");
+   }
+   public static void lyric4()
+   {
+      System.out.println("She swallowed the bird to catch the spider,");
+   }
+   public static void lyric6()
+   {
+      System.out.println("She swallowed the cat to catch the bird,");
+   }
+   public static void lyric8()
+   {
+      System.out.println("She swallowed the dog to catch the cat,");
+   }
+
+}
